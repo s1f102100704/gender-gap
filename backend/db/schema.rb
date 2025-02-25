05977da55b0b1f8d90eb20af02eda0ddb4ac147f
1F10210096 <s1f102100962@iniad.org>
2025-02-25 23:34:01 +0900

    dockerの環境構築

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_02_25_082805) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
