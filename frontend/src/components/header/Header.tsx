ccffb23623c4c9c21f1fd6cffc007525141625ec
s1f102100704 <s1f102100704@iniad.org>
2025-02-28 01:14:45 +0900

    merge

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,13 +1,15 @@
 
 import styles from "./header.module.css"
+import maleImg from "../../assets/male.png"
+import femaleImg from "../../assets/female.png"
 const Header =()=>{
     return (
         <div className={styles.header}>
             <div className={styles.banner}>
-                <div className={styles.title}>男女論.com</div>
+                <div className={styles.title}><img src={maleImg}/><img src ={femaleImg}/></div>
             </div>
-            <div className={styles.sushi}>
-                男女論を毎日おしゃべり♪
+            <div className={styles.kaiten}>
+                <div className={styles.sushi}>男女論を毎日おしゃべり</div>
             </div>
         </div>
 
