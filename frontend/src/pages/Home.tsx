8c57c83627f92581fc9b9e8ad2010473e998633a
s1f102100704 <s1f102100704@iniad.org>
2025-02-28 06:10:55 +0900

    createTopic画面の見た目ある程度

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { getUsers } from "../services/userService";
 import styles from "./home.module.css"
-import Header from "../components/header/Header";
-import MainTopic from "../components/contents/mainTopic/MainTopic";
-import SubTopic from "../components/contents/subTopic/SubTopic";
-import KaitenSushi from "../components/kaitenSushi/KaitenSushi";
+import Header from "../components/Home/header/Header";
+import MainTopic from "../components/Home/contents/mainTopic/MainTopic";
+import SubTopic from "../components/Home/contents/subTopic/SubTopic";
+import KaitenSushi from "../components/Home/kaitenSushi/KaitenSushi";
 
 const Home = () => {
     const [users, setUsers] = useState<{ id: number; name: string }[]>([]);
