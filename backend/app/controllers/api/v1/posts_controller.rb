2e3f905cd95ebad7bf3f56c50db63f2fe2e53d5b
s1f102100704 <s1f102100704@iniad.org>
2025-03-07 16:25:01 +0900

    show ではなくindexが呼ばれていた

--- backend/app/controllers/api/v1/posts_controller.rb
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -3,16 +3,22 @@ module Api
         class Api::V1::PostsController < ApplicationController
 
             def index
-                contents = Post.fetch_recent
-                render_json_response(contents)
-            end
-            def show
-                content = Post.find_by(discussion_thread_id:params[:discussion_thread_id])
+                content = Post.where(discussion_thread_id: params[:discussion_thread_id])
                 if content
                     render_json_response(content)
                 else
                     render json: { error: "Post not found for this discussion_thread_id" }, status: :not_found
                 end
+                
+            end
+            def show
+                content = Post.where(discussion_thread_id: params[:discussion_thread_id])
+                if content
+                    render_json_response(content)
+                else
+                    render json: { error: "Post not found for this discussion_thread_id" }, status: :not_found
+                end
+                
             end
             def create
                 content = Post.build_new(content_params)
