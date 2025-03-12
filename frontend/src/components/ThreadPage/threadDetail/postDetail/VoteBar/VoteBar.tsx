700e59fd320c5c16ced062a362d430da63c9b86d
s1f102100704 <s1f102100704@iniad.org>
2025-03-12 21:14:29 +0900

    緊急事態発生

--- frontend/src/components/ThreadPage/threadDetail/postDetail/VoteBar/VoteBar.tsx
+++ frontend/src/components/ThreadPage/threadDetail/postDetail/VoteBar/VoteBar.tsx
@@ -1,19 +1,21 @@
 import  { useState } from 'react'
 import styles from "./voteBar.module.css"
+import usePostVoteCounts from '../../../../../hook/threadDetail/usePostVoteCounts';
 interface VoteProps {
     initialUpvotes: number;
     initialDownvotes: number;
   }
 const VoteBar = ({ initialUpvotes, initialDownvotes }: VoteProps) => {
+    const {postVotes} = usePostVoteCounts();
     const [upvotes, setUpvotes] = useState(initialUpvotes);
     const [downvotes, setDownvotes] = useState(initialDownvotes);
     const totalVotes = upvotes + downvotes;
     const upvoteRatio = totalVotes > 0 ? (upvotes / totalVotes) * 100 : 50;
     const downvoteRatio = totalVotes > 0 ? (downvotes / totalVotes) * 100 : 50;
-    console.log(upvoteRatio)
+
     return (
       <div className={styles.voteContainer}>
-        <button className={styles.upvoteButton} onClick={() => setUpvotes(upvotes + 1)}>
+        <button className={styles.upvoteButton} onClick={() => postVotes(1)}>
           +
         </button>
         <div className={styles.voteBarContainer}>
@@ -22,7 +24,7 @@ const VoteBar = ({ initialUpvotes, initialDownvotes }: VoteProps) => {
             <div className={styles.downvoteBar} style={{ width: `${downvoteRatio}%` }}></div>
           </div>
         </div>
-        <button className={styles.downvoteButton} onClick={() => setDownvotes(downvotes + 1)}>
+        <button className={styles.downvoteButton} onClick={() => postVotes(2)}>
           -
         </button>
       </div>
