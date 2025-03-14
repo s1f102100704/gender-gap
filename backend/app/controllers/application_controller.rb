c9e1193f45b58fe112dd1c6a38349e9a0f77406c
s1f102100704 <s1f102100704@iniad.org>
2025-03-14 21:25:46 +0900

    ipアドレスのhash化した

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -13,10 +13,16 @@ class ApplicationController < ActionController::API
     render json: { status: 'error', message: message }, status: status
   end
   private
-    
+  
+  def hashed_ip(ip)
+    salt = 'kankyohensu' +Date.today.to_s  # 環境変数から取得
+    Digest::SHA256.hexdigest(salt + ip)
+  end
+
   def set_current_user
     ip = request.remote_ip
-    @current_user = User.find_or_create_by(ip_address: ip)
+    hashed_ip = hashed_ip(ip)
+    @current_user = User.find_or_create_by(ip_address: hashed_ip)
   end
 end
 
