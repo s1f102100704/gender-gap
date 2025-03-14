2b973e2c748771804abfe4725a6b8a4dff46178b
s1f102100704 <s1f102100704@iniad.org>
2025-03-15 02:01:00 +0900

    こっちでvotes昨日少しプッシュ

--- frontend/src/hook/threadDetail/usePostVoteCounts.ts
+++ frontend/src/hook/threadDetail/usePostVoteCounts.ts
@@ -1,10 +1,16 @@
 import axios from 'axios'
-import { VOTES_API_URL } from '../../config'
-import { useState } from 'react'
 
 const usePostVoteCounts = () => {
-    const postVotes= (votes_type:1|2) =>{
-        const response = axios.post(VOTES_API_URL,votes_type)
+    const postVotes= (vote_type:1|2, post_id:string) =>{
+      const votesUrl = `http://localhost:3000/api/v1/posts/${post_id}/votes`
+      try{
+        const response = axios.post(votesUrl,{vote:{vote_type:vote_type,post_id:post_id}})
+        console.log("response:",response)
+      }catch(err){
+        console.log(err)
+      }
+     
+      
     }
   return {postVotes}
 }
