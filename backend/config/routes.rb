bc0edfedbfb0692b91ea7ee2741ea18d37460f67
1F10210096 <s1f102100962@iniad.org>
2025-03-18 20:23:31 +0900

    修正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show, :create]
       resources :discussion_threads, only: [:index, :show, :create]
-      resources :discussion_threads_populartopic, only: [:index, :show, :create]
-      resources :discussion_threads_newtopic, only: [:index, :show, :create]
+      resources :discussion_threads_populartopic, only: [:index]
+      resources :discussion_threads_newtopic, only: [:index]
       resources :posts, only: [:index, :show, :create], defaults: { format: :json } do
         resources :votes, only: [:index, :create, :destroy]
       end
