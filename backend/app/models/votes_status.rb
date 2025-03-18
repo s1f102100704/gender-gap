f4464afb927396f8367b39d279d7d31012ce993b
s1f102100704 <s1f102100704@iniad.org>
2025-03-19 03:09:32 +0900

    キャッシュ削除の設計をserviceとjobで分けた

--- backend/app/models/votes_status.rb
+++
@@ -1,3 +0,0 @@
-class VotesStatus < ApplicationRecord
-  belongs_to :post
-end
---
+++ backend/app/services/vote_cache_service.rb
@@ -0,0 +1,22 @@
+class VoteCacheService
+  def self.get_votes(post_id)
+    redis_key = "post_#{post_id}_votes"
+    cached_votes = $redis.get(redis_key)
+    return JSON.parse(cached_votes) if cached_votes
+
+    # キャッシュがない場合はDBから取得
+    post = Post.find(post_id)
+    votes = Vote.vote_counts(post)
+
+    # キャッシュに保存（10分間）
+    $redis.set(redis_key, votes.to_json)
+    $redis.expire(redis_key, 10.minutes.to_i)
+
+    votes
+  end
+
+  def self.clear_cache(post_id)
+    redis_key = "post_#{post_id}_votes"
+    $redis.del(redis_key)
+  end
+end
