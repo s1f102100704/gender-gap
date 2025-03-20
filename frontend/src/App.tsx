3a319b7920079446e44a82b1d147f40b29d3f1da
1F10210096 <s1f102100962@iniad.org>
2025-03-20 14:16:47 +0900

    ログイン画面フロント作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MakeTopic from "./pages/MakeTopic";
 import ThreadPage from "./pages/ThreadPage";
+import Login from "./pages/admin/Admin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Login />} />
         <Route path="/make_topic" element={<MakeTopic />} />
         <Route path="threads/:id" element={<ThreadPage />} />
       </Routes>
