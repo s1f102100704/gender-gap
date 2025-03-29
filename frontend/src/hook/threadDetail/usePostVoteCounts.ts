f2366453e6b173e46cfb3dd2ba63bb64a2e845c4
1F10210096 <s1f102100962@iniad.org>
2025-03-29 12:37:55 +0900

    voteに性別を保存

--- frontend/src/hook/threadDetail/usePostVoteCounts.ts
+++ frontend/src/hook/threadDetail/usePostVoteCounts.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { useCallback } from "react";
+import { useGenderLogin } from "../gender/useGenderLogin";
 
 const usePostVoteCounts = (post_id: string) => {
+  const { getValidGender } = useGenderLogin();
   const postVotes = async (vote_type: 1 | -1) => {
     const votesUrl = `http://localhost:3000/api/v1/posts/${post_id}/votes`;
     try {
       const res = await axios.post(votesUrl, {
-        vote: { vote_type: vote_type, post_id: post_id },
+        vote: { vote_type: vote_type, post_id: post_id, gender: getValidGender() },
       });
       console.log("susi", res.data);
     } catch (err) {
