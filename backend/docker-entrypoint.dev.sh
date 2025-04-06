c16924320a8bd452c601a1552798d25be8a12cc2
s1f102100704 <s1f102100704@iniad.org>
2025-04-06 19:07:28 +0900

    いったん

---
+++ backend/docker-entrypoint.dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+export RAILS_ENV=development
+# PIDファイルが残っていたら削除
+rm -f tmp/pids/server.pid
+
+# DBが使えるようになるまで待つ
+echo "Waiting for database to be ready..."
+until bin/rails db:migrate; do
+  echo "Database unavailable, retrying in 2s..."
+  sleep 2
+done
+
+echo "✅ Database ready. Starting server..."
+exec "$@"
